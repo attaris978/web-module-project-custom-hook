56bf8670e6d2c181cfe37d1edb04c74c9f161b23
unknown <ryancare@protonmail.com>
2022-04-13 10:09:55 -0500

    Imported useLocalStorage into useDarkMode and used it in place of useState

--- frontend/hooks/useDarkMode.js
+++ frontend/hooks/useDarkMode.js
@@ -1,8 +1,15 @@
-import {useState} from 'react';
+// import {useState} from 'react';
+import useLocalStorage from './useLocalStorage';
+
+// const useDarkMode = (initialValue) => {
+//     const [darkMode,setDarkMode] = useState(initialValue);
+//     return [darkMode,setDarkMode];
+// };
 
 const useDarkMode = (initialValue) => {
-    const [darkMode,setDarkMode] = useState(initialValue);
+    const [darkMode,setDarkMode] = useLocalStorage(initialValue);
     return [darkMode,setDarkMode];
 };
+
 
 export default useDarkMode;
